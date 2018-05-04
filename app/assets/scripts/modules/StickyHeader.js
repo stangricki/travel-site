1270ce8686ec1550d2048951b765fc57a4e184fe
Filip Stangricki <stangricki@gmail.com>
2018-05-04 08:24:25 +0200

    Completed browser support

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -4,7 +4,7 @@ import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
   constructor() {
-    this.lazyImages = $(".lazyload")
+    this.lazyImages = $(".lazyload");
     this.siteHeader = $(".site-header");
     this.headerTriggerElement = $(".large-hero__title");
     this.createHeaderWaypoint();
@@ -16,9 +16,9 @@ class StickyHeader {
   }
 
   refreshWaypoints() {
-    this.lazyImages.on('load', function() {
+    this.lazyImages.load(function() {
       Waypoint.refreshAll();
-    })
+    });
   }
 
   addSmoothScrolling() {
